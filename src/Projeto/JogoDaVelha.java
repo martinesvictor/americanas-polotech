935fad33ec6197747a5aae5960bc1b10b95d42c0
Victor <victorhmatines@gmail.com>
2022-12-18 17:49:59 -0300

    implentei ganhador e condições para posições

--- src/Projeto/JogoDaVelha.java
+++ src/Projeto/JogoDaVelha.java
@@ -9,30 +9,52 @@ public class JogoDaVelha {
 
         Character[][] matriz = new Character[3][3];
         Scanner scanner = new Scanner(System.in);
+        int linha, coluna;
+        boolean acabou = false;
 
         for (int i = 0; i < 9; i++) {
+            int jogador;
             imprimirTabuleiro(matriz);
             if (i % 2 == 0) {
-                System.out.println("---------- Jogador 1 'O' ----------");
+                jogador = 1;
+                System.out.printf("---------- Jogador %d 'O' ----------\n", jogador);
             } else {
-                System.out.println("---------- Jogador 2 'X' ----------");
+                jogador = 2;
+                System.out.printf("---------- Jogador %d 'X' ----------\n", jogador);
             }
-            System.out.print("Escolha a posição da Linha [Horizontal]: ");
-            int linha = scanner.nextInt();
-            while (linha > 2 || linha < 0) {
-                System.out.print("Posição inválida. Digite novamente [0, 1, 2]: ");
+            do {
+                System.out.print("Escolha a posição da Linha [Horizontal]: ");
                 linha = scanner.nextInt();
-            }
-            System.out.print("Escolha a posição da Coluna [Vertical]: ");
-            int coluna = scanner.nextInt();
-            while (coluna > 2 || coluna < 0) {
-                System.out.print("Posição inválida. Digite novamente [0, 1, 2]: ");
+
+                System.out.print("Escolha a posição da Coluna [Vertical]: ");
                 coluna = scanner.nextInt();
-            }
+            } while (linha < 0 || linha > 2 || coluna < 0 || coluna > 2 || matriz[linha][coluna] != null);
+
             if (i % 2 == 0) {
                 matriz[linha][coluna] = 'O';
             } else {
                 matriz[linha][coluna] = 'X';
+            }
+            if (i > 3) {
+                for (int j = 0; j < 3; j++) {
+                    for (int k = 0; k < 3; k++) {
+                        if ((matriz[k][0] == matriz[k][1] && matriz[k][1] == matriz[k][2]) ||
+                                (matriz[0][k] == matriz[1][k] && matriz[1][k] == matriz[2][k]) ||
+                                (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2]) ||
+                                (matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0])) {
+                            acabou = true;
+                            break;
+                        }
+                    }
+                    if (acabou == true) {
+                        break;
+                    }
+                }
+            }
+            if (acabou == true) {
+                imprimirTabuleiro(matriz);
+                System.out.printf("* * * * O Jogador %d '%s' GANHOU!! * * * *\n", jogador, matriz[linha][coluna]);
+                break;
             }
         }
     }
